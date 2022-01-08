14cde85980ddec2c4ca0935368e1ca63e00fd492
Do Jin-uk <96456991+dozinq@users.noreply.github.com>
2022-01-08 22:45:01 +0900

    Create 9498.py

---
+++ BOJ/9498.py
@@ -0,0 +1,13 @@
+score = int(input())
+
+if score >= 90 :
+    grade = 'A'
+elif score >= 80 :
+    grade = 'B'
+elif score >= 70 :
+    grade = 'C'
+elif score >= 60 :
+    grade = 'D'
+else :
+    grade = 'F'
+print('%s' % (grade))
